7e8a5ed0d1769c9960ffba518002256016fc4a9e
yejin lee <ingkejin@gmail.com>
2023-03-06 14:25:14 +0900

    ⚡️ useState를 활용한 값 변경

--- reactbasic/src/App.js
+++ reactbasic/src/App.js
@@ -2,14 +2,22 @@
 import logo from './logo.svg';
 import './App.css';
 import State from './component/State'
-import {userState} from 'react'
+import {useState} from 'react'
 
 function App() {
   let num = 0;
-  
+  const [count, setCount] = useState(0)
+  let increase = () => {
+    num = num + 1;
+    setCount(count + 1)
+    console.log("num", num, "state", count)
+  }
+
   return (
     <div className='App'>
-
+      {/* <State/> */}
+      <h1>{count}</h1>
+      <button onClick={increase}>+1</button>
     </div>
   );
 }
