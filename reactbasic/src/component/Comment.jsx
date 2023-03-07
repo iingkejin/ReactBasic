6b19f6793b13d6cbef74ce5384ecda951e0faf10
yejin lee <ingkejin@gmail.com>
2023-03-07 14:55:38 +0900

    ⚡️ const styles로 style 적용

--- reactbasic/src/component/Comment.jsx
+++ reactbasic/src/component/Comment.jsx
@@ -2,19 +2,47 @@ import React from 'react';
 // import './Comment.css';
 
 const styles = {
-  
+  wrapper : {
+    display : 'flex',
+    margin: '8px',
+    padding : '8px',
+    border : '1px solid #ccc',
+    borderRadius : '16px',
+  },
+
+  image : {
+    width : '50px',
+    height: '50px',
+    borderRadius : '50%',
+  },
+
+  contentContainer : {
+    display : 'flex',
+    flexDirection : 'column',
+    justifyContent : 'center',
+    marginLeft : '8px',
+  },
+
+  nameText : {
+    fontSize : '18px',
+    fontWeight : 'bold',
+  },
+
+  commentText : {
+    fontSize : '14px',
+  }
 }
 
 function Comment(props) {
   return (
-    <div className='wrapper'>
-      <div className='img-box'>
-        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="user-img" />
+    <div style={styles.wrapper}>
+      <div>
+        <img style={styles.image} src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="user-img" />
       </div>
 
-      <div className='content-container'>
-        <span className='name-text'>{props.name}</span>
-        <span className='comment-text'>{props.comment}</span>
+      <div style={styles.contentContainer}>
+        <span style={styles.nameText}>{props.name}</span>
+        <span style={styles.commentText}>{props.comment}</span>
       </div>
     </div>
   )
