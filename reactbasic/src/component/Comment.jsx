a93f66884d3d77107bb2def302432fe89a02198b
yejin lee <ingkejin@gmail.com>
2023-03-07 11:34:33 +0900

    💄 Comment Component 생성 및 스타일

---
+++ reactbasic/src/component/Comment.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Comment.css';
+
+function Comment(props) {
+  return (
+    <div className='wrapper'>
+      <div className='img-box'>
+        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="user-img" />
+      </div>
+
+      <div className='content-container'>
+        <span className='name-text'>{props.name}</span>
+        <span className='comment-text'>{props.comment}</span>
+      </div>
+    </div>
+  )
+}
+
+export default Comment;
