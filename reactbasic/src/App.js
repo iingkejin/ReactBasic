d9cce5a516b38cf5b663bb9141c7a07d7ffa1226
yejin lee <ingkejin@gmail.com>
2023-03-09 10:27:19 +0900

    ✅ img, backgroundImage

--- reactbasic/src/App.js
+++ reactbasic/src/App.js
@@ -3,13 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 import Comment from './component/Comment';
 import CommentList from './component/CommentList'
+import back from './toy.jpg'
 
 function App() {
   return (
     <div className='App'>
       <CommentList/>
+      {/* <div className='back'></div> */}
+      {/* <div style={{backgroundImage : 'url(' + back + ')', height : '400px'}}></div> */}
+      {/* <div style={{backgroundImage :` url(${back})`, height : '400px'}}></div>
+      <img src={process.env.PUBLIC_URL + 'toy2.jpg'} /> */}
+
+      {/* <div style={{backgroundImage : 'url(https://m.media-amazon.com/images/M/MV5BMTgxOTY4Mjc0MF5BMl5BanBnXkFtZTcwNTA4MDQyMw@@._V1_FMjpg_UX1000_.jpg)', height : '300px'}}></div> */}
+      {/* <img src="https://m.media-amazon.com/images/M/MV5BMTgxOTY4Mjc0MF5BMl5BanBnXkFtZTcwNTA4MDQyMw@@._V1_FMjpg_UX1000_.jpg" alt="" /> */}
+      <img src={back} alt="" />
+
     </div>
   );
 }
-
 export default App;
